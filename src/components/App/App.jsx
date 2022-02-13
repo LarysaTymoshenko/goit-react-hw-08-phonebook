ebe1d033ec2acb11f8a313c253b684d1ba44cbaa
LarysaTymoshenko <luda76@ukr.net>
2022-02-13 16:02:17 +0200

    1602

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,14 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy, Suspense, useEffect } from 'react';
 import Loader from '../Loader/Loader';
 import UserMenu from '../UserMenu/UserMenu';
-import LoginPage from '../../pages/login';
-import RegisterPage from '../../pages/register';
+import LoginPage from '../../pages/LoginPage';
+import RegisterPage from '../../pages/RegisterPage';
 import { useGetUserQuery } from '../../redux/users/users-reducer';
 import { onError } from '../../utilits/toast';
 import PublicRoute from '../PublicRoute/PublicRoute';
 import PrivateRoute from '../PrivatRoute/PrivetRoute';
 
-const ContactsPage = lazy(() => import('../../pages/contacts'))
+const ContactsPage = lazy(() => import('../../pages/ContactsPage'))
 
 export default function App() {
   const { error } = useGetUserQuery()
